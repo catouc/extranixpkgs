8af2b6ccc013173a86a3bf353fbc69dfe9fa8634
Philipp Böschen <catouc@philipp.boeschen.me>
2024-03-25 21:32:14 +0100

    Add a first version of the firefly-iii package
    It compiles and will eventually be used to properly set up an accounting
    server for my home.

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
   in {
     packages.x86_64-linux.ytdl-sub = pkgs.callPackage ./packages/ytdl-sub.nix { };
+    packages.x86_64-linux.firefly-iii = pkgs.callPackage ./packages/firefly-iii.nix { };
     nixosModules."ytdl-sub" = import ./nixos-module.nix;
   };
 }
